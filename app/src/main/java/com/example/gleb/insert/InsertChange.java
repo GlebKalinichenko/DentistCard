dba2d947b48720f018ff3717b38dc49ceb6a2f13
GlebKalinichenko <Glebjn@yandex.ua>
2015-06-20 19:02:39 +0300

    Activity for add new record on table changes

--- app/src/main/java/com/example/gleb/insert/InsertChange.java
+++ app/src/main/java/com/example/gleb/insert/InsertChange.java
@@ -1,7 +1,84 @@
 package com.example.gleb.insert;
 
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.gleb.dentistcard.R;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+
 /**
  * Created by gleb on 20.06.15.
  */
-public class InsertChange {
+public class InsertChange extends InsertPattern {
+    private EditText changeEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.insert_change);
+
+        changeEditText = (EditText) findViewById(R.id.changeEditText);
+        insertButton = (Button) findViewById(R.id.insertChangeButton);
+        insertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Sender().execute();
+            }
+        });
+    }
+
+    public class Sender extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            String change = changeEditText.getText().toString();
+            Log.d(TAG, change);
+
+            client = new DefaultHttpClient();
+            post = new HttpPost("http://dentists.16mb.com/InsertQuery/InsertChangeScript.php");
+            HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000); // Timeout
+            HttpResponse response;
+            JSONObject json = new JSONObject();
+
+            try {
+                json.put("changing", change);
+                post.setHeader("json", json.toString());
+                StringEntity se = new StringEntity(json.toString());
+                se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+                post.setEntity(se);
+                response = client.execute(post);
+
+                if (response != null) {
+                    InputStream in = response.getEntity().getContent(); // Get the
+                    Log.i("Read from Server", in.toString());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Toast.makeText(getBaseContext(), R.string.Send, Toast.LENGTH_SHORT).show();
+        }
+    }
 }
