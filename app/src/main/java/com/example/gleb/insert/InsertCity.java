8abe9d13471dbc8fe2b1bb7c5cb096cbffb365dc
GlebKalinichenko <Glebjn@yandex.ua>
2015-06-20 16:31:35 +0300

    Activity for insert city and layout for it

---
+++ app/src/main/java/com/example/gleb/insert/InsertCity.java
@@ -0,0 +1,19 @@
+package com.example.gleb.insert;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+/**
+ * Created by gleb on 20.06.15.
+ */
+public class InsertCity extends InsertPattern {
+    private EditText countryKodEditText;
+    private EditText cityEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
