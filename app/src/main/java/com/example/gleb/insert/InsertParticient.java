07a6f6bfaac019dcada1d4cd5ee4203610ef2627
GlebKalinichenko <Glebjn@yandex.ua>
2015-06-21 19:15:34 +0300

    Activity for insert new record in table particient

---
+++ app/src/main/java/com/example/gleb/insert/InsertParticient.java
@@ -0,0 +1,104 @@
+package com.example.gleb.insert;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.gleb.dentistcard.R;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+
+/**
+ * Created by gleb on 21.06.15.
+ */
+public class InsertParticient extends InsertPattern {
+    private EditText fioEditText;
+    private EditText cityKodEditText;
+    private EditText addressEditText;
+    private EditText phoneNumberEditText;
+    private EditText dateBornEditText;
+    private EditText fioParentEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.insert_particient);
+
+        fioEditText = (EditText) findViewById(R.id.fioEditText);
+        cityKodEditText = (EditText) findViewById(R.id.cityKodEditText);
+        addressEditText = (EditText) findViewById(R.id.addressEditText);
+        phoneNumberEditText = (EditText) findViewById(R.id.phoneNumberEditText);
+        dateBornEditText = (EditText) findViewById(R.id.dateBornEditText);
+        fioParentEditText = (EditText) findViewById(R.id.fioParentEditText);
+        insertButton = (Button) findViewById(R.id.insertParticientButton);
+        insertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Sender().execute();
+            }
+        });
+    }
+
+    public class Sender extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            String fio = fioEditText.getText().toString();
+            String cityKod = cityKodEditText.getText().toString();
+            String address = addressEditText.getText().toString();
+            String phoneNumber = phoneNumberEditText.getText().toString();
+            String dateBorn = dateBornEditText.getText().toString();
+            String fioParent = fioParentEditText.getText().toString();
+
+            Log.d(TAG, fio);
+
+            client = new DefaultHttpClient();
+            post = new HttpPost("http://dentists.16mb.com/InsertQuery/InsertParticientScript.php");
+            HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000); // Timeout
+            HttpResponse response;
+            JSONObject json = new JSONObject();
+
+            try {
+                json.put("fio", fio);
+                json.put("cityKod", cityKod);
+                json.put("address", address);
+                json.put("phoneNumber", phoneNumber);
+                json.put("dateBorn", dateBorn);
+                json.put("fioParent", fioParent);
+                post.setHeader("json", json.toString());
+                StringEntity se = new StringEntity(json.toString());
+                se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+                post.setEntity(se);
+                response = client.execute(post);
+
+                if (response != null) {
+                    InputStream in = response.getEntity().getContent(); // Get the
+                    Log.i("Read from Server", in.toString());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Toast.makeText(getBaseContext(), R.string.Send, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
