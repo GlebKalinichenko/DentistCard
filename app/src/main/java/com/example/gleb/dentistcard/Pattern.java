68600eaf10fd0ccae870b982b252e1389fc86724
GlebKalinichenko <Glebjn@yandex.ua>
2015-06-25 17:37:57 +0300

    Add variable for set query

--- app/src/main/java/com/example/gleb/dentistcard/Pattern.java
+++ app/src/main/java/com/example/gleb/dentistcard/Pattern.java
@@ -3,7 +3,11 @@ package com.example.gleb.dentistcard;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
 
 /**
  * Created by Gleb on 05.06.2015.
@@ -11,5 +15,8 @@ import android.widget.ListView;
 abstract class Pattern extends ActionBarActivity {
     protected ListView listView;
     protected ArrayAdapter<String> adapter;
+    protected HttpClient client;
+    protected HttpPost post;
+    protected ImageButton imageButton;
 
 }
