b14a213cc2135dd61f491aaf695709234d6ac3d9
GlebKalinichenko <Glebjn@yandex.ua>
2015-06-25 20:18:30 +0300

    Updating and deleting query for table Diagnoses

--- app/src/main/java/com/example/gleb/dentistcard/Diagnoses.java
+++ app/src/main/java/com/example/gleb/dentistcard/Diagnoses.java
@@ -1,32 +1,54 @@
 package com.example.gleb.dentistcard;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ActionMode;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.gleb.adapters.DiagnoseAdapter;
 import com.example.gleb.adapters.ParticientAdapter;
 import com.example.gleb.insert.InsertChange;
+import com.example.gleb.insert.InsertCountry;
 import com.example.gleb.insert.InsertDiagnose;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.protocol.HTTP;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.util.ArrayList;
 
 /**
  * Created by Gleb on 07.06.2015.
  */
 public class Diagnoses extends Pattern {
     public static final String TAG = "TAG";
-    private int[] arrayIdDiagnoses = null;
-    private String[] arrayDiagnoses = null;
+    public int[] arrayIdDiagnoses = null;
+    public String[] arrayDiagnoses = null;
     private DatabaseRequest request = new DatabaseRequest();
+    public EditText oldDiagnoseEditText;
+    public EditText newDiagnoseEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +59,104 @@ public class Diagnoses extends Pattern {
         View header = (View) getLayoutInflater().inflate(R.layout.viewheaderdiagnoses, null);
         listView.addHeaderView(header);
         new Loader().execute();
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                /**
+                 * View for delete record
+                 */
+                AlertDialog.Builder builder = new AlertDialog.Builder(Diagnoses.this);
+                View dialogView = LayoutInflater.from(Diagnoses.this).inflate(R.layout.update_diagnose, null);
+                builder.setView(dialogView);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+                oldDiagnoseEditText = (EditText) dialog.findViewById(R.id.oldDiagnoseEditText);
+                oldDiagnoseEditText.append(arrayDiagnoses[position - 1]);
+                newDiagnoseEditText = (EditText) dialog.findViewById(R.id.newDiagnoseEditText);
+
+                imageButton = (ImageButton) dialog.findViewById(R.id.updateImageButton);
+                imageButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.d(TAG, "Click");
+                        new Updater().execute();
+                    }
+                });
+            }
+        });
+
+        listView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE_MODAL);
+        listView.setMultiChoiceModeListener(new MultiChoice(listView));
+    }
+
+    /**
+     * MultiChoice for delete record and add new record in table Diagnoses
+     */
+    public class MultiChoice implements AbsListView.MultiChoiceModeListener {
+        private AbsListView list;
+        public ArrayList<Integer> diagnoses = new ArrayList<Integer>();
+
+        public MultiChoice(AbsListView list) {
+            this.list = list;
+        }
+
+        @Override
+        public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+            Log.d(TAG, "OnItemCheck");
+            if (checked) {
+                int rows = list.getCheckedItemCount();
+                diagnoses.add(position);
+                setSubtitle(mode, rows);
+            }
+        }
+
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            MenuInflater inflater = mode.getMenuInflater();
+            inflater.inflate(R.menu.context_menu, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            if (item.getItemId() == R.id.item_add) {
+                Log.d(TAG, "Item add");
+                Intent intent = new Intent(getBaseContext(), InsertCountry.class);
+                startActivity(intent);
+            } else {
+                if (item.getItemId() == R.id.item_delete) {
+                    for (int i = 0; i < diagnoses.size(); i++) {
+                        Log.d(TAG, arrayDiagnoses[diagnoses.get(i) - 1]);
+                        new Deleter(diagnoses.get(i) - 1).execute();
+
+                    }
+                }
+            }
+            return false;
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+
+        }
+
+        private void setSubtitle(ActionMode mode, int selectedCount) {
+            switch (selectedCount) {
+                case 0:
+                    mode.setSubtitle(null);
+                    break;
+                default:
+                    mode.setTitle(String.valueOf(selectedCount));
+                    break;
+            }
+        }
     }
 
     @Override
@@ -61,6 +181,99 @@ public class Diagnoses extends Pattern {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Update record from table Diagnoses
+     */
+    public class Updater extends AsyncTask<String, String, String>{
+
+        @Override
+        protected String doInBackground(String... params) {
+            String oldDiagnose = oldDiagnoseEditText.getText().toString();
+            String newDiagnose = newDiagnoseEditText.getText().toString();
+
+            Log.d(TAG, "Old Diagnose " + oldDiagnose);
+            Log.d(TAG, "New Diagnose " + newDiagnose);
+
+            client = new DefaultHttpClient();
+            post = new HttpPost("http://dentists.16mb.com/UpdateScript/UpdateDiagnoseScript.php");
+            HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000); // Timeout
+            HttpResponse response;
+            JSONObject json = new JSONObject();
+
+            try {
+                json.put("oldDiagnose", oldDiagnose);
+                json.put("newDiagnose", newDiagnose);
+                post.setHeader("json", json.toString());
+                StringEntity se = new StringEntity(json.toString());
+                se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+                post.setEntity(se);
+                response = client.execute(post);
+
+                if (response != null) {
+                    InputStream in = response.getEntity().getContent(); // Get the
+                    Log.i("Read from Server", in.toString());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Toast.makeText(getBaseContext(), R.string.Send, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Delete record from table diagnoses
+     */
+    public class Deleter extends AsyncTask<String, String, String>{
+        private int positionDiagnose;
+
+        public Deleter(int positionDiagnose) {
+            this.positionDiagnose = positionDiagnose;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String diagnose = arrayDiagnoses[positionDiagnose];
+            Log.d(TAG, diagnose);
+
+            client = new DefaultHttpClient();
+            post = new HttpPost("http://dentists.16mb.com/DeleteScript/DeleteDiagnoseScript.php");
+            HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000); // Timeout
+            HttpResponse response;
+            JSONObject json = new JSONObject();
+
+            try {
+                json.put("diagnose", diagnose);
+                post.setHeader("json", json.toString());
+                StringEntity se = new StringEntity(json.toString());
+                se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+                post.setEntity(se);
+                response = client.execute(post);
+
+                if (response != null) {
+                    InputStream in = response.getEntity().getContent(); // Get the
+                    Log.i("Read from Server", in.toString());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Toast.makeText(getBaseContext(), R.string.Send, Toast.LENGTH_SHORT).show();
+        }
     }
 
     public class Loader extends AsyncTask<String, String, String[]> {
