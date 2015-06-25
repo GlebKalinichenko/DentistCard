7e4deffa5f6a697e4f985233e4f8f1dc8c5cc423
GlebKalinichenko <Glebjn@yandex.ua>
2015-06-25 17:34:31 +0300

    Adding update and delete record from listview, add content actionbar with call activities for add new record and delete record

--- app/src/main/java/com/example/gleb/dentistcard/Changes.java
+++ app/src/main/java/com/example/gleb/dentistcard/Changes.java
@@ -1,22 +1,42 @@
 package com.example.gleb.dentistcard;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ActionMode;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.gleb.adapters.ChangeAdapter;
 import com.example.gleb.insert.InsertChange;
 import com.example.gleb.insert.InsertCity;
+import com.example.gleb.insert.InsertCountry;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.protocol.HTTP;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.util.ArrayList;
 
 /**
  * Created by Gleb on 06.06.2015.
@@ -27,6 +47,8 @@ public class Changes extends Pattern {
     private ChangeAdapter adapter;
     private String[] arrayChanges = null;
     private int[] arrayIdChanges = null;
+    public EditText oldChangeEditText;
+    public EditText newChangeEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +60,197 @@ public class Changes extends Pattern {
         listView.addHeaderView(header);
         new Loader().execute();
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                /**
+                 * View for delete record
+                 */
+                AlertDialog.Builder builder = new AlertDialog.Builder(Changes.this);
+                View dialogView = LayoutInflater.from(Changes.this).inflate(R.layout.update_change, null);
+                builder.setView(dialogView);
+                AlertDialog dialog = builder.create();
+                dialog.show();
 
+                oldChangeEditText = (EditText) dialog.findViewById(R.id.oldChangeEditText);
+                oldChangeEditText.append(String.valueOf(arrayChanges[position - 1]));
+                newChangeEditText = (EditText) dialog.findViewById(R.id.newChangeEditText);
+
+                imageButton = (ImageButton) dialog.findViewById(R.id.updateImageButton);
+                imageButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.d(TAG, "Click");
+                        new Updater().execute();
+                    }
+                });
+            }
+        });
+
+        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+        listView.setMultiChoiceModeListener(new MultiChoice(listView));
+    }
+
+    /**
+     * MultiChoice for delete record and add new record in table Changes
+     */
+    public class MultiChoice implements AbsListView.MultiChoiceModeListener{
+        private AbsListView list;
+        public ArrayList<Integer> changes = new ArrayList<Integer>();
+
+        public MultiChoice(AbsListView list) {
+            this.list = list;
+        }
+
+        @Override
+        public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+            Log.d(TAG, "OnItemCheck");
+            if (checked) {
+                int rows = list.getCheckedItemCount();
+                changes.add(position);
+                setSubtitle(mode, rows);
+            }
+        }
+
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            MenuInflater inflater = mode.getMenuInflater();
+            inflater.inflate(R.menu.context_menu, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            if (item.getItemId() == R.id.item_add){
+                Log.d(TAG, "Item add");
+                Intent intent = new Intent(getBaseContext(), InsertCountry.class);
+                startActivity(intent);
+            }
+            else{
+                if (item.getItemId() == R.id.item_delete){
+                    for (int i = 0; i < changes.size(); i++){
+                        Log.d(TAG, arrayChanges[changes.get(i) - 1]);
+                        new Deleter(changes.get(i) - 1).execute();
+
+                    }
+                }
+            }
+            return false;
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+
+        }
+
+        private void setSubtitle(ActionMode mode, int selectedCount) {
+            switch (selectedCount) {
+                case 0:
+                    mode.setSubtitle(null);
+                    break;
+                default:
+                    mode.setTitle(String.valueOf(selectedCount));
+                    break;
+            }
+        }    /**
+         * Delete record from table Changes
+         */
+        public class Deleter extends AsyncTask<String, String, String>{
+            private int positionChange;
+
+            public Deleter(int positionChange) {
+                this.positionChange = positionChange;
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+                String change = arrayChanges[positionChange];
+                Log.d(TAG, change);
+
+                client = new DefaultHttpClient();
+                post = new HttpPost("http://dentists.16mb.com/DeleteScript/DeleteChangeScript.php");
+                HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000); // Timeout
+                HttpResponse response;
+                JSONObject json = new JSONObject();
+
+                try {
+                    json.put("change", change);
+                    post.setHeader("json", json.toString());
+                    StringEntity se = new StringEntity(json.toString());
+                    se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+                    post.setEntity(se);
+                    response = client.execute(post);
+
+                    if (response != null) {
+                        InputStream in = response.getEntity().getContent(); // Get the
+                        Log.i("Read from Server", in.toString());
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                Toast.makeText(getBaseContext(), R.string.Send, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+
+    /**
+     * Update record from table Changes
+     */
+    public class Updater extends AsyncTask<String, String, String>{
+
+        @Override
+        protected String doInBackground(String... params) {
+            String oldChange = oldChangeEditText.getText().toString();
+            String newChange = newChangeEditText.getText().toString();
+
+            Log.d(TAG, "Old Change " + oldChange);
+            Log.d(TAG, "New Change " + newChange);
+
+            client = new DefaultHttpClient();
+            post = new HttpPost("http://dentists.16mb.com/UpdateScript/UpdateChangeScript.php");
+            HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000); // Timeout
+            HttpResponse response;
+            JSONObject json = new JSONObject();
+
+            try {
+                json.put("oldChange", oldChange);
+                json.put("newChange", newChange);
+                post.setHeader("json", json.toString());
+                StringEntity se = new StringEntity(json.toString());
+                se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+                post.setEntity(se);
+                response = client.execute(post);
+
+                if (response != null) {
+                    InputStream in = response.getEntity().getContent(); // Get the
+                    Log.i("Read from Server", in.toString());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Toast.makeText(getBaseContext(), R.string.Send, Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
