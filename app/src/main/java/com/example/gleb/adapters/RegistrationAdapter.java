3549352a9838f35ee771760d3de0dec78f1f7dac
GlebKalinchenko <Glebjn@yandex.ua>
2015-06-30 10:37:29 +0300

    Adapter change fields kod ParticientKod on lookup fields with record from table Particients

--- app/src/main/java/com/example/gleb/adapters/RegistrationAdapter.java
+++ app/src/main/java/com/example/gleb/adapters/RegistrationAdapter.java
@@ -15,13 +15,13 @@ import com.example.gleb.dentistcard.R;
 public class RegistrationAdapter extends ArrayAdapter<String> {
     public static final String TAG = "TAG";
     public String[] arrayDateRegistration;
-    public int[] arrayParticientKod;
+    public String[] arrayParticientKod;
     public Context context;
     public TextView dateRegistrationTextView;
     public TextView particientKodTextView;
     private LayoutInflater mInflater;
 
-    public RegistrationAdapter(Context context, String[] arrayDateRegistration, int[] arrayParticientKod) {
+    public RegistrationAdapter(Context context, String[] arrayDateRegistration, String[] arrayParticientKod) {
         super(context, R.layout.registration_item_row, arrayDateRegistration);
         this.context = context;
         this.arrayDateRegistration = arrayDateRegistration;
@@ -31,7 +31,7 @@ public class RegistrationAdapter extends ArrayAdapter<String> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        View view;  // создаем itemView из заданного layout
+        View view;  // пїЅпїЅпїЅпїЅпїЅпїЅпїЅ itemView пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ layout
         if (convertView == null) {
             view = mInflater.inflate(R.layout.registration_item_row, parent, false);
         } else {
