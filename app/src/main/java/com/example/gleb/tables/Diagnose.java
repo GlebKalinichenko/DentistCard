af61966215b80175c7f945c726b717aba82781a7
GlebKalinichenko <Glebjn@yandex.ua>
2015-07-13 15:09:47 +0300

    Description table Diagnoses

---
+++ app/src/main/java/com/example/gleb/tables/Diagnose.java
@@ -0,0 +1,14 @@
+package com.example.gleb.tables;
+
+/**
+ * Created by gleb on 08.07.15.
+ */
+public class Diagnose {
+    public int idDiagnose;
+    public String diagnose;
+
+    public Diagnose(int idDiagnose, String diagnose) {
+        this.idDiagnose = idDiagnose;
+        this.diagnose = diagnose;
+    }
+}
