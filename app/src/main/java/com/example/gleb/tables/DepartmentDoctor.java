aac77baf17de8437ee1b320493d95c210c2bfb4b
GlebKalinichenko <Glebjn@yandex.ua>
2015-07-13 15:09:23 +0300

    Description table DepartmentDoctors

---
+++ app/src/main/java/com/example/gleb/tables/DepartmentDoctor.java
@@ -0,0 +1,14 @@
+package com.example.gleb.tables;
+
+/**
+ * Created by gleb on 07.07.15.
+ */
+public class DepartmentDoctor {
+    public int idDepartmentDoctor;
+    public String departmentDoctor;
+
+    public DepartmentDoctor(int idDepartmentDoctor, String departmentDoctor) {
+        this.idDepartmentDoctor = idDepartmentDoctor;
+        this.departmentDoctor = departmentDoctor;
+    }
+}
