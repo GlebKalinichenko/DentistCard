38cb1971db3f6837358cec74eb41aeaa69efe112
GlebKalinichenko <Glebjn@yandex.ua>
2015-07-13 15:16:10 +0300

    Adapter for show content from table Kvalifications

--- app/src/main/java/com/example/gleb/adapters/KvalificationAdapter.java
+++ app/src/main/java/com/example/gleb/adapters/KvalificationAdapter.java
@@ -1,43 +1,90 @@
 package com.example.gleb.adapters;
 
-import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.example.gleb.dentistcard.R;
+import com.example.gleb.tables.Kvalification;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Created by Gleb on 07.06.2015.
+ * Created by gleb on 08.07.15.
  */
-public class KvalificationAdapter extends ArrayAdapter<String> {
-    public static final String TAG = "TAG";
-    public Context context;
-    private String[] kvalifications;
-    public TextView kvalificationTextView;
-    private LayoutInflater mInflater;
+public class KvalificationAdapter extends RecyclerView.Adapter<KvalificationAdapter.KvalificationViewHolder> {
+    private SparseBooleanArray selectedItems;
+    private List<Kvalification> kvalifications;
 
-    public KvalificationAdapter(Context context, String[] kvalifications) {
-        super(context, R.layout.kvalification_item_row, kvalifications);
-        this.context = context;
+    public static class KvalificationViewHolder extends RecyclerView.ViewHolder {
+        TextView kvalificationTextView;
+
+        KvalificationViewHolder(View itemView) {
+            super(itemView);
+            kvalificationTextView = (TextView)itemView.findViewById(R.id.kvalification);
+        }
+    }
+
+    public KvalificationAdapter(List<Kvalification> kvalifications){
         this.kvalifications = kvalifications;
-        this.mInflater = LayoutInflater.from(context);
     }
 
     @Override
-         public View getView(int position, View convertView, ViewGroup parent) {
-        View view;  // ������� itemView �� ��������� layout
-        if (convertView == null) {
-            view = mInflater.inflate(R.layout.kvalification_item_row, parent, false);
-        } else {
-            view = convertView;
-        }
-
-        kvalificationTextView = (TextView) view.findViewById(R.id.kvalificationTextView);
-        kvalificationTextView.setText(kvalifications[position]);
-
-        return view;
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
     }
+
+    @Override
+    public KvalificationViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_kvalification, viewGroup, false);
+        KvalificationViewHolder pvh = new KvalificationViewHolder(v);
+        return pvh;
+    }
+
+    @Override
+    public void onBindViewHolder(KvalificationViewHolder kvalificationViewHolder, int i) {
+        kvalificationViewHolder.kvalificationTextView.setText(kvalifications.get(i).kvalification);
+    }
+
+    public void removeData(int position) {
+        kvalifications.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return kvalifications.size();
+    }
+
+    public void toggleSelection(int pos) {
+        if (selectedItems.get(pos, false)) {
+            selectedItems.delete(pos);
+        }
+        else {
+            selectedItems.put(pos, true);
+        }
+        notifyItemChanged(pos);
+    }
+
+    public void clearSelections() {
+        selectedItems.clear();
+        notifyDataSetChanged();
+    }
+
+    public int getSelectedItemCount() {
+        return selectedItems.size();
+    }
+
+    public List<Integer> getSelectedItems() {
+        List<Integer> items = new ArrayList<Integer>(selectedItems.size());
+        for (int i = 0; i < selectedItems.size(); i++) {
+            items.add(selectedItems.keyAt(i));
+        }
+        return items;
+    }
+
 }
