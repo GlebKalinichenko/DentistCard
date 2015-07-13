4add16b5fdfe5af90550373d6b81e5e32bd3f3db
GlebKalinichenko <Glebjn@yandex.ua>
2015-07-13 15:17:26 +0300

    Adapter for show content from table Timetables

--- app/src/main/java/com/example/gleb/adapters/TimetableAdapter.java
+++ app/src/main/java/com/example/gleb/adapters/TimetableAdapter.java
@@ -1,57 +1,97 @@
 package com.example.gleb.adapters;
 
-import android.content.Context;
-import android.util.Log;
+import android.support.v7.widget.RecyclerView;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.example.gleb.dentistcard.R;
+import com.example.gleb.tables.Timetable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Created by Gleb on 07.06.2015.
+ * Created by gleb on 09.07.15.
  */
-public class TimetableAdapter extends ArrayAdapter<String> {
+public class TimetableAdapter extends RecyclerView.Adapter<TimetableAdapter.TimetableViewHolder> {
     public static final String TAG = "TAG";
-    public Context context;
-    public String[] dateWork;
-    public int[] doctorsKod;
-    public int[] changesKod;
-    private LayoutInflater mInflater;
-    public TextView dateWorkTextView;
-    public TextView doctorKodTextView;
-    public TextView changeKodTextView;
+    private SparseBooleanArray selectedItems;
+    List<Timetable> timetables;
 
+    public static class TimetableViewHolder extends RecyclerView.ViewHolder {
+        TextView dateWorkTextView;
+        TextView doctorKodTextView;
+        TextView changeKodTextView;
 
-    public TimetableAdapter(Context context, String[] dateWork, int[] doctorsKod, int[] changesKod) {
-        super(context, R.layout.timetable_item_row, dateWork);
-        this.context = context;
-        this.dateWork = dateWork;
-        this.changesKod = changesKod;
-        this.doctorsKod = doctorsKod;
-        this.mInflater = LayoutInflater.from(context);
-        Log.d(TAG, "Adapter timetable" + String.valueOf(dateWork[0]));
+        TimetableViewHolder(View itemView) {
+            super(itemView);
+            doctorKodTextView = (TextView)itemView.findViewById(R.id.doctorKod);
+            changeKodTextView = (TextView)itemView.findViewById(R.id.changeKod);
+            dateWorkTextView = (TextView)itemView.findViewById(R.id.dateWork);
+        }
+    }
+
+    public TimetableAdapter(List<Timetable> timetables){
+        this.timetables = timetables;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View view;  // создаем itemView из заданного layout
-        if (convertView == null) {
-            view = mInflater.inflate(R.layout.timetable_item_row, parent, false);
-        } else {
-            view = convertView;
-        }
-
-        dateWorkTextView = (TextView) view.findViewById(R.id.dateWorkTextView);
-        doctorKodTextView = (TextView) view.findViewById(R.id.doctorKodTextView);
-        changeKodTextView = (TextView) view.findViewById(R.id.changeKodTextView);
-
-        dateWorkTextView.setText(dateWork[position]);
-        doctorKodTextView.setText(String.valueOf(doctorsKod[position]));
-        changeKodTextView.setText(String.valueOf(changesKod[position]));
-
-        return view;
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
     }
+
+    @Override
+    public TimetableViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_timetable, viewGroup, false);
+        TimetableViewHolder pvh = new TimetableViewHolder(v);
+        return pvh;
+    }
+
+    @Override
+    public void onBindViewHolder(TimetableViewHolder timetableViewHolder, int i) {
+        timetableViewHolder.doctorKodTextView.setText(timetables.get(i).doctorKod);
+        timetableViewHolder.changeKodTextView.setText(timetables.get(i).changeKod);
+        timetableViewHolder.dateWorkTextView.setText(timetables.get(i).dateWork);
+    }
+
+    public void removeData(int position) {
+        timetables.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return timetables.size();
+    }
+
+    public void toggleSelection(int pos) {
+        if (selectedItems.get(pos, false)) {
+            selectedItems.delete(pos);
+        }
+        else {
+            selectedItems.put(pos, true);
+        }
+        notifyItemChanged(pos);
+    }
+
+    public void clearSelections() {
+        selectedItems.clear();
+        notifyDataSetChanged();
+    }
+
+    public int getSelectedItemCount() {
+        return selectedItems.size();
+    }
+
+    public List<Integer> getSelectedItems() {
+        List<Integer> items = new ArrayList<Integer>(selectedItems.size());
+        for (int i = 0; i < selectedItems.size(); i++) {
+            items.add(selectedItems.keyAt(i));
+        }
+        return items;
+    }
+
 }
