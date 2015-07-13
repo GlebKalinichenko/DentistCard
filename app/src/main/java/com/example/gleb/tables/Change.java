b51991921b751f438325e98a75443414706c1119
GlebKalinichenko <Glebjn@yandex.ua>
2015-07-13 15:08:29 +0300

    Description table Change

---
+++ app/src/main/java/com/example/gleb/tables/Change.java
@@ -0,0 +1,14 @@
+package com.example.gleb.tables;
+
+/**
+ * Created by gleb on 07.07.15.
+ */
+public class Change {
+    public int idChange;
+    public String changing;
+
+    public Change(int idChange, String changing) {
+        this.idChange = idChange;
+        this.changing = changing;
+    }
+}
