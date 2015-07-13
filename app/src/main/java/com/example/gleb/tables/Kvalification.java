e5575f31ce99d76344228c327a98176b1ebedc9c
GlebKalinichenko <Glebjn@yandex.ua>
2015-07-13 15:10:23 +0300

    Description table Kvalifications

---
+++ app/src/main/java/com/example/gleb/tables/Kvalification.java
@@ -0,0 +1,14 @@
+package com.example.gleb.tables;
+
+/**
+ * Created by gleb on 08.07.15.
+ */
+public class Kvalification {
+    public int idKvalification;
+    public String kvalification;
+
+    public Kvalification(int idKvalification, String kvalification) {
+        this.idKvalification = idKvalification;
+        this.kvalification = kvalification;
+    }
+}
