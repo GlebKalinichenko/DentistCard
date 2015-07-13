7f9d751b8edbf1bdac6f710cf5f678d9c6c00686
GlebKalinichenko <Glebjn@yandex.ua>
2015-07-13 15:10:55 +0300

    Description table Posts

---
+++ app/src/main/java/com/example/gleb/tables/Post.java
@@ -0,0 +1,14 @@
+package com.example.gleb.tables;
+
+/**
+ * Created by gleb on 08.07.15.
+ */
+public class Post {
+    public int idPost;
+    public String post;
+
+    public Post(int idPost, String post) {
+        this.idPost = idPost;
+        this.post = post;
+    }
+}
