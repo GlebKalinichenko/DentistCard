4f91fd869feccc9fa348161c1a76de0259752180
GlebKalinichenko <Glebjn@yandex.ua>
2015-07-13 15:14:43 +0300

    Adapter for show content from table Changes

--- app/src/main/java/com/example/gleb/adapters/ChangeAdapter.java
+++ app/src/main/java/com/example/gleb/adapters/ChangeAdapter.java
@@ -1,45 +1,90 @@
 package com.example.gleb.adapters;
 
-import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.example.gleb.dentistcard.R;
+import com.example.gleb.tables.Change;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Created by Gleb on 07.06.2015.
+ * Created by gleb on 07.07.15.
  */
-public class ChangeAdapter extends ArrayAdapter<String> {
-    public static final String TAG = "TAG";
-    public Context context;
-    public TextView changeTextView;
-    public String[] changes;
-    private LayoutInflater mInflater;
+public class ChangeAdapter extends RecyclerView.Adapter<ChangeAdapter.ChangeViewHolder> {
+    private SparseBooleanArray selectedItems;
+    private List<Change> changes;
 
-    public ChangeAdapter(Context context, String[] changes) {
-        super(context, R.layout.changes_item_row, changes);
-        this.context = context;
+    public static class ChangeViewHolder extends RecyclerView.ViewHolder {
+        TextView changeTextView;
+
+        ChangeViewHolder(View itemView) {
+            super(itemView);
+            changeTextView = (TextView)itemView.findViewById(R.id.change);
+        }
+    }
+
+    public ChangeAdapter(List<Change> changes){
         this.changes = changes;
-        this.mInflater = LayoutInflater.from(context);
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View view;
-        if (convertView == null) {
-            view = mInflater.inflate(R.layout.changes_item_row, parent, false);
-        } else {
-            view = convertView;
-        }
-
-        changeTextView = (TextView) view.findViewById(R.id.changeTextView);
-        changeTextView.setText(changes[position]);
-
-        return view;
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
     }
 
+    @Override
+    public ChangeViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_change, viewGroup, false);
+        ChangeViewHolder pvh = new ChangeViewHolder(v);
+        return pvh;
+    }
+
+    @Override
+    public void onBindViewHolder(ChangeViewHolder changeViewHolder, int i) {
+        changeViewHolder.changeTextView.setText(changes.get(i).changing);
+    }
+
+    public void removeData(int position) {
+        changes.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return changes.size();
+    }
+
+    public void toggleSelection(int pos) {
+        if (selectedItems.get(pos, false)) {
+            selectedItems.delete(pos);
+        }
+        else {
+            selectedItems.put(pos, true);
+        }
+        notifyItemChanged(pos);
+    }
+
+    public void clearSelections() {
+        selectedItems.clear();
+        notifyDataSetChanged();
+    }
+
+    public int getSelectedItemCount() {
+        return selectedItems.size();
+    }
+
+    public List<Integer> getSelectedItems() {
+        List<Integer> items = new ArrayList<Integer>(selectedItems.size());
+        for (int i = 0; i < selectedItems.size(); i++) {
+            items.add(selectedItems.keyAt(i));
+        }
+        return items;
+    }
 
 }
