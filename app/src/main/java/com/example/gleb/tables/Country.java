5fff60ec53cbeca6b9f4635313a51002f50a61f2
GlebKalinichenko <Glebjn@yandex.ua>
2015-07-13 15:09:01 +0300

    Description table Countries

---
+++ app/src/main/java/com/example/gleb/tables/Country.java
@@ -0,0 +1,14 @@
+package com.example.gleb.tables;
+
+/**
+ * Created by gleb on 04.07.15.
+ */
+public class Country {
+    public String country;
+    public int idCountry;
+
+    public Country(int idCountry, String country) {
+        this.idCountry = idCountry;
+        this.country = country;
+    }
+}
