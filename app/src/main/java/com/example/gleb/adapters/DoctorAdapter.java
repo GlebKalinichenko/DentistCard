604204b2314ce7d85a439d28351aa8ca18c9ad5d
GlebKalinichenko <Glebjn@yandex.ua>
2015-07-13 15:15:58 +0300

    Adapter for show content from table Doctors

--- app/src/main/java/com/example/gleb/adapters/DoctorAdapter.java
+++ app/src/main/java/com/example/gleb/adapters/DoctorAdapter.java
@@ -1,68 +1,103 @@
 package com.example.gleb.adapters;
 
-import android.content.Context;
-import android.util.Log;
+import android.support.v7.widget.RecyclerView;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.example.gleb.dentistcard.R;
+import com.example.gleb.tables.Doctor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Created by Gleb on 07.06.2015.
+ * Created by gleb on 10.07.15.
  */
-public class DoctorAdapter extends ArrayAdapter<String> {
+public class DoctorAdapter extends RecyclerView.Adapter<DoctorAdapter.DoctorViewHolder> {
     public static final String TAG = "TAG";
-    private Context context;
-    private String[] FIO;
-    private int[] postKod;
-    private int[] departmentKod;
-    private int[] kvalificationKod;
-    private int[] expirience;
-    private LayoutInflater mInflater;
-    public TextView FIOTextView;
-    public TextView postKodTextView;
-    public TextView kvalificationKodTextView;
-    public TextView departmentKodTextView;
-    public TextView expirienceTextView;
+    private SparseBooleanArray selectedItems;
+    List<Doctor> doctors;
 
-    public DoctorAdapter(Context context, String[] FIO, int[] postKod, int[] kvalificationKod, int[] departmentKod, int[] expirience) {
-        super(context, R.layout.doctor_item_row, FIO);
-        this.context = context;
-        this.FIO = FIO;
-        this.postKod = postKod;
-        this.departmentKod = departmentKod;
-        this.kvalificationKod = kvalificationKod;
-        this.expirience = expirience;
-        this.mInflater = LayoutInflater.from(context);
-        Log.d(TAG, "Adapter doctor " + String.valueOf(postKod[0]));
+    public static class DoctorViewHolder extends RecyclerView.ViewHolder {
+        TextView FIOTextView;
+        TextView postKodTextView;
+        TextView kvalificationKodTextView;
+        TextView departmentKodTextView;
+        TextView expirienceTextView;
+
+        DoctorViewHolder(View itemView) {
+            super(itemView);
+            FIOTextView = (TextView)itemView.findViewById(R.id.FIO);
+            postKodTextView = (TextView)itemView.findViewById(R.id.postKod);
+            kvalificationKodTextView = (TextView)itemView.findViewById(R.id.kvalificationKod);
+            departmentKodTextView = (TextView)itemView.findViewById(R.id.departmentKod);
+            expirienceTextView = (TextView)itemView.findViewById(R.id.expirience);
+        }
+    }
+
+    public DoctorAdapter(List<Doctor> doctors){
+        this.doctors = doctors;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View view;  // ÒÓÁ‰‡ÂÏ itemView ËÁ Á‡‰‡ÌÌÓ„Ó layout
-        if (convertView == null) {
-            view = mInflater.inflate(R.layout.doctor_item_row, parent, false);
-        } else {
-            view = convertView;
-        }
-
-        FIOTextView = (TextView) view.findViewById(R.id.FIOTextView);
-        kvalificationKodTextView = (TextView) view.findViewById(R.id.KvalificationKodTextView);
-        departmentKodTextView = (TextView) view.findViewById(R.id.DepartmentKodTextView);
-        postKodTextView = (TextView) view.findViewById(R.id.PostKodTextView);
-        expirienceTextView = (TextView) view.findViewById(R.id.ExpirienceTextView);
-
-        FIOTextView.setText(FIO[position]);
-        kvalificationKodTextView.setText(String.valueOf(kvalificationKod[position]));
-        departmentKodTextView.setText(String.valueOf(departmentKod[position]));
-
-        postKodTextView.setText(String.valueOf(postKod[position]));
-        expirienceTextView.setText(String.valueOf(expirience[position]));
-
-
-        return view;
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
     }
+
+    @Override
+    public DoctorViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_doctor, viewGroup, false);
+        DoctorViewHolder pvh = new DoctorViewHolder(v);
+        return pvh;
+    }
+
+    @Override
+    public void onBindViewHolder(DoctorViewHolder doctorViewHolder, int i) {
+        doctorViewHolder.FIOTextView.setText(doctors.get(i).FIO);
+        doctorViewHolder.postKodTextView.setText(doctors.get(i).postKod);
+        doctorViewHolder.kvalificationKodTextView.setText(doctors.get(i).kvalificationKod + " –∫–≤–∞–ª–∏—Ñ–∏–∫–∞—Ü–∏—è");
+        doctorViewHolder.departmentKodTextView.setText(doctors.get(i).departmentKod);
+        doctorViewHolder.expirienceTextView.setText(String.valueOf(doctors.get(i).expirience + " –ª–µ—Ç –æ–ø—ã—Ç–∞"));
+    }
+
+    public void removeData(int position) {
+        doctors.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return doctors.size();
+    }
+
+    public void toggleSelection(int pos) {
+        if (selectedItems.get(pos, false)) {
+            selectedItems.delete(pos);
+        }
+        else {
+            selectedItems.put(pos, true);
+        }
+        notifyItemChanged(pos);
+    }
+
+    public void clearSelections() {
+        selectedItems.clear();
+        notifyDataSetChanged();
+    }
+
+    public int getSelectedItemCount() {
+        return selectedItems.size();
+    }
+
+    public List<Integer> getSelectedItems() {
+        List<Integer> items = new ArrayList<Integer>(selectedItems.size());
+        for (int i = 0; i < selectedItems.size(); i++) {
+            items.add(selectedItems.keyAt(i));
+        }
+        return items;
+    }
+
 }
