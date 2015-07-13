31be7a694448a35b661bf33602a9d02fda89b1d4
GlebKalinichenko <Glebjn@yandex.ua>
2015-07-13 15:00:05 +0300

    Activity for navigate between activities of system

--- app/src/main/java/com/example/gleb/dentistcard/Navigator.java
+++ app/src/main/java/com/example/gleb/dentistcard/Navigator.java
@@ -1,12 +1,26 @@
 package com.example.gleb.dentistcard;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.mikepenz.iconics.typeface.FontAwesome;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SectionDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.Badgeable;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.Nameable;
 
 /**
  * Created by Gleb on 07.06.2015.
@@ -26,11 +40,12 @@ public class Navigator extends ActionBarActivity {
     private Button recommendationButton;
     private Button postButton;
 
+    private Drawer.Result drawerResult = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.navigate);
+        setContentView(R.layout.navigator_drawer);
 
         countryButton = (Button) findViewById(R.id.countryButton);
         cityButton = (Button) findViewById(R.id.cityButton);
@@ -49,7 +64,7 @@ public class Navigator extends ActionBarActivity {
         countryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), Countries.class);
+                Intent intent = new Intent(getBaseContext(), CountryActivity.class);
                 startActivity(intent);
             }
         });
@@ -57,7 +72,7 @@ public class Navigator extends ActionBarActivity {
         cityButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), Cities.class);
+                Intent intent = new Intent(getBaseContext(), CityActivity.class);
                 startActivity(intent);
             }
         });
@@ -65,7 +80,7 @@ public class Navigator extends ActionBarActivity {
         departmentButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), DepartmentsDoctors.class);
+                Intent intent = new Intent(getBaseContext(), DepartmentDoctorActivity.class);
                 startActivity(intent);
             }
         });
@@ -73,7 +88,7 @@ public class Navigator extends ActionBarActivity {
         changeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), Changes.class);
+                Intent intent = new Intent(getBaseContext(), ChangeActivity.class);
                 startActivity(intent);
             }
         });
@@ -81,7 +96,7 @@ public class Navigator extends ActionBarActivity {
         kvalificationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), Kvalifications.class);
+                Intent intent = new Intent(getBaseContext(), KvalificationActivity.class);
                 startActivity(intent);
             }
         });
@@ -89,7 +104,7 @@ public class Navigator extends ActionBarActivity {
         timetableButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), Timetables.class);
+                Intent intent = new Intent(getBaseContext(), TimetableActivity.class);
                 startActivity(intent);
             }
         });
@@ -97,7 +112,7 @@ public class Navigator extends ActionBarActivity {
         doctorButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), Doctors.class);
+                Intent intent = new Intent(getBaseContext(), DoctorActivity.class);
                 startActivity(intent);
             }
         });
@@ -105,7 +120,7 @@ public class Navigator extends ActionBarActivity {
         particientsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), Particients.class);
+                Intent intent = new Intent(getBaseContext(), ParticientActivity.class);
                 startActivity(intent);
             }
         });
@@ -113,7 +128,7 @@ public class Navigator extends ActionBarActivity {
         diagnoseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), Diagnoses.class);
+                Intent intent = new Intent(getBaseContext(), DiagnoseActivity.class);
                 startActivity(intent);
 
             }
@@ -122,7 +137,7 @@ public class Navigator extends ActionBarActivity {
         registrationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), Registrations.class);
+                Intent intent = new Intent(getBaseContext(), RegistrationActivity.class);
                 startActivity(intent);
             }
         });
@@ -130,7 +145,7 @@ public class Navigator extends ActionBarActivity {
         ticketButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), Tickets.class);
+                Intent intent = new Intent(getBaseContext(), TicketActivity.class);
                 startActivity(intent);
             }
         });
@@ -138,7 +153,7 @@ public class Navigator extends ActionBarActivity {
         recommendationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), Recommendations.class);
+                Intent intent = new Intent(getBaseContext(), RecycleView.class);
                 startActivity(intent);
             }
         });
@@ -146,9 +161,82 @@ public class Navigator extends ActionBarActivity {
         postButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), Posts.class);
+                Intent intent = new Intent(getBaseContext(), PostActivity.class);
                 startActivity(intent);
             }
         });
+
+        // Инициализируем Toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Инициализируем Navigation Drawer
+        Drawer drawer = new Drawer();
+        drawer.withActivity(this);
+        drawer.withToolbar(toolbar);
+        drawer.withActionBarDrawerToggle(true);
+        drawer.withHeader(R.layout.drawer_header);
+
+        drawer.addDrawerItems(
+                new PrimaryDrawerItem().withName(R.string.drawer_item_home).withIcon(FontAwesome.Icon.faw_home).withBadge("99").withIdentifier(1),
+                new PrimaryDrawerItem().withName(R.string.drawer_item_free_play).withIcon(FontAwesome.Icon.faw_gamepad),
+                new PrimaryDrawerItem().withName(R.string.drawer_item_custom).withIcon(FontAwesome.Icon.faw_eye).withBadge("6").withIdentifier(2),
+                new SectionDrawerItem().withName(R.string.drawer_item_settings),
+                new SecondaryDrawerItem().withName(R.string.drawer_item_help).withIcon(FontAwesome.Icon.faw_cog),
+                new SecondaryDrawerItem().withName(R.string.drawer_item_open_source).withIcon(FontAwesome.Icon.faw_question).setEnabled(false),
+                new DividerDrawerItem(),
+                new SecondaryDrawerItem().withName(R.string.drawer_item_contact).withIcon(FontAwesome.Icon.faw_github).withBadge("12+").withIdentifier(1)
+        );
+
+        drawer.withOnDrawerListener(new Drawer.OnDrawerListener() {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Скрываем клавиатуру при открытии Navigation Drawer
+                InputMethodManager inputMethodManager = (InputMethodManager) Navigator.this.getSystemService(Activity.INPUT_METHOD_SERVICE);
+                inputMethodManager.hideSoftInputFromWindow(Navigator.this.getCurrentFocus().getWindowToken(), 0);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+            }
+        });
+
+        drawer.withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+            @Override
+            // Обработка клика
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id, IDrawerItem drawerItem) {
+                if (drawerItem instanceof Nameable) {
+                    Toast.makeText(Navigator.this, Navigator.this.getString(((Nameable) drawerItem).getNameRes()), Toast.LENGTH_SHORT).show();
+                }
+                if (drawerItem instanceof Badgeable) {
+                    Badgeable badgeable = (Badgeable) drawerItem;
+                    if (badgeable.getBadge() != null) {
+                        // учтите, не делайте так, если ваш бейдж содержит символ "+"
+                        try {
+                            int badge = Integer.valueOf(badgeable.getBadge());
+                            if (badge > 0) {
+                                drawerResult.updateBadge(String.valueOf(badge - 1), position);
+                            }
+                        } catch (Exception e) {
+                            Log.d("test", "Не нажимайте на бейдж, содержащий плюс! :)");
+                        }
+                    }
+                }
+            }
+        });
+
+        drawer.withOnDrawerItemLongClickListener(new Drawer.OnDrawerItemLongClickListener() {
+            @Override
+            // Обработка длинного клика, например, только для SecondaryDrawerItem
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id, IDrawerItem drawerItem) {
+                if (drawerItem instanceof SecondaryDrawerItem) {
+                    Toast.makeText(Navigator.this, Navigator.this.getString(((SecondaryDrawerItem) drawerItem).getNameRes()), Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+        });
+        drawer.build();
     }
+
 }
