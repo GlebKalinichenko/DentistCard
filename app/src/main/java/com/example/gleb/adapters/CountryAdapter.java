9891c217a031c9b189f717a2b125c8b7b3193cb9
GlebKalinichenko <Glebjn@yandex.ua>
2015-07-13 15:15:09 +0300

    Adapter for show content from table Countries

--- app/src/main/java/com/example/gleb/adapters/CountryAdapter.java
+++ app/src/main/java/com/example/gleb/adapters/CountryAdapter.java
@@ -1,49 +1,90 @@
 package com.example.gleb.adapters;
 
-import android.app.Activity;
-import android.content.Context;
-import android.util.Log;
+import android.support.v7.widget.RecyclerView;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import com.example.gleb.tables.Country;
 import com.example.gleb.dentistcard.R;
 
-/**
- * Created by Gleb on 05.06.2015.
- */
-public class CountryAdapter extends ArrayAdapter<String> {
-    public Context context;
-    public String[] values;
-    public TextView countryTextView;
-    public static final String TAG = "TAG";
-    private LayoutInflater mInflater;
+import java.util.ArrayList;
+import java.util.List;
 
-    public CountryAdapter(Context context, String[] values) {
-        super(context, R.layout.countries_item_row, values);
-        this.context = context;
-        this.values = values;
-        this.mInflater = LayoutInflater.from(context);
-        Log.d(TAG, values[0]);
+/**
+ * Created by gleb on 04.07.15.
+ */
+public class CountryAdapter extends RecyclerView.Adapter<CountryAdapter.CountryViewHolder>{
+    private SparseBooleanArray selectedItems;
+
+    public static class CountryViewHolder extends RecyclerView.ViewHolder {
+        TextView countryTextView;
+
+        CountryViewHolder(View itemView) {
+            super(itemView);
+            countryTextView = (TextView)itemView.findViewById(R.id.country);
+        }
+    }
+
+    List<Country> countries;
+
+    public CountryAdapter(List<Country> countries){
+        this.countries = countries;
     }
 
     @Override
-         public View getView(int position, View convertView, ViewGroup parent) {
-        View view;  // создаем itemView из заданного layout
-        if (convertView == null) {
-            view = mInflater.inflate(R.layout.countries_item_row, parent, false);
-        } else {
-            view = convertView;
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    @Override
+    public CountryViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_country, viewGroup, false);
+        CountryViewHolder pvh = new CountryViewHolder(v);
+        return pvh;
+    }
+
+    @Override
+    public void onBindViewHolder(CountryViewHolder CountryViewHolder, int i) {
+        CountryViewHolder.countryTextView.setText(countries.get(i).country);
+    }
+
+    public void removeData(int position) {
+        countries.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return countries.size();
+    }
+
+    public void toggleSelection(int pos) {
+        if (selectedItems.get(pos, false)) {
+            selectedItems.delete(pos);
         }
+        else {
+            selectedItems.put(pos, true);
+        }
+        notifyItemChanged(pos);
+    }
 
-        countryTextView = (TextView) view.findViewById(R.id.countryTextView);
+    public void clearSelections() {
+        selectedItems.clear();
+        notifyDataSetChanged();
+    }
 
+    public int getSelectedItemCount() {
+        return selectedItems.size();
+    }
 
-
-        countryTextView.setText(values[position]);
-
-        return view;
+    public List<Integer> getSelectedItems() {
+        List<Integer> items = new ArrayList<Integer>(selectedItems.size());
+        for (int i = 0; i < selectedItems.size(); i++) {
+            items.add(selectedItems.keyAt(i));
+        }
+        return items;
     }
 }
