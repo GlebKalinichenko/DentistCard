bfe871e54b60883d970396e298f1b3ecff1deb08
GlebKalinichenko <Glebjn@yandex.ua>
2015-07-13 15:08:44 +0300

    Description table Cities

---
+++ app/src/main/java/com/example/gleb/tables/City.java
@@ -0,0 +1,14 @@
+package com.example.gleb.tables;
+
+/**
+ * Created by gleb on 05.07.15.
+ */
+public class City {
+    public String city;
+    public String countryKod;
+
+    public City(String city, String countryKod) {
+        this.city = city;
+        this.countryKod = countryKod;
+    }
+}
