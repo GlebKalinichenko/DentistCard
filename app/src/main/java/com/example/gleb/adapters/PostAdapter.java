973722a8c548ac687b864ab454b4f4ea820f9a01
GlebKalinichenko <Glebjn@yandex.ua>
2015-07-13 15:16:33 +0300

    Adapter for show content from table Posts

--- app/src/main/java/com/example/gleb/adapters/PostAdapter.java
+++ app/src/main/java/com/example/gleb/adapters/PostAdapter.java
@@ -1,46 +1,93 @@
 package com.example.gleb.adapters;
 
-import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.example.gleb.dentistcard.R;
+import com.example.gleb.tables.Post;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Created by gleb on 20.06.15.
+ * Created by gleb on 08.07.15.
  */
-public class PostAdapter extends ArrayAdapter<String> {
-    public static final String TAG = "TAG";
-    public Context context;
-    public String[] arrayPost;
-    private LayoutInflater mInflater;
-    public TextView postTextView;
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder> {
+    private SparseBooleanArray selectedItems;
+    private List<Post> posts;
 
+    public static class PostViewHolder extends RecyclerView.ViewHolder {
+        TextView postTextView;
 
-    public PostAdapter(Context context, String[] arrayPost) {
-        super(context, R.layout.post_item_row, arrayPost);
-        this.context = context;
-        this.arrayPost = arrayPost;
-        this.mInflater = LayoutInflater.from(context);
+        PostViewHolder(View itemView) {
+            super(itemView);
+            postTextView = (TextView)itemView.findViewById(R.id.post);
+        }
+    }
+
+    public PostAdapter(List<Post> posts){
+        this.posts = posts;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View view;
-        if (convertView == null) {
-            view = mInflater.inflate(R.layout.post_item_row, parent, false);
-        } else {
-            view = convertView;
-        }
-
-        postTextView = (TextView) view.findViewById(R.id.postTextView);
-        postTextView.setText(arrayPost[position]);
-
-        return view;
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
     }
 
+    @Override
+    public PostViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_post, viewGroup, false);
+        PostViewHolder pvh = new PostViewHolder(v);
+        return pvh;
+    }
+
+    @Override
+    public void onBindViewHolder(PostViewHolder postViewHolder, int i) {
+        postViewHolder.postTextView.setText(posts.get(i).post);
+    }
+
+    public void removeData(int position) {
+        posts.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return posts.size();
+    }
+
+    public void toggleSelection(int pos) {
+        if (selectedItems.get(pos, false)) {
+            selectedItems.delete(pos);
+        }
+        else {
+            selectedItems.put(pos, true);
+        }
+        notifyItemChanged(pos);
+    }
+
+    public void clearSelections() {
+        selectedItems.clear();
+        notifyDataSetChanged();
+    }
+
+    public int getSelectedItemCount() {
+        return selectedItems.size();
+    }
+
+    public List<Integer> getSelectedItems() {
+        List<Integer> items = new ArrayList<Integer>(selectedItems.size());
+        for (int i = 0; i < selectedItems.size(); i++) {
+            items.add(selectedItems.keyAt(i));
+        }
+        return items;
+    }
 
 }
+
+
+
