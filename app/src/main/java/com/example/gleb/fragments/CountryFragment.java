84d47a2dced552fa1fbd2b94ce87edf228f930c1
GlebKalinichenko <Glebjn@yandex.ua>
2015-07-19 12:05:58 +0300

    Fragment for show records from table Countries in tab Country

---
+++ app/src/main/java/com/example/gleb/fragments/CountryFragment.java
@@ -0,0 +1,301 @@
+package com.example.gleb.fragments;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.ActionMode;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.example.gleb.adapters.CountryAdapter;
+import com.example.gleb.dentistcard.CountryActivity;
+import com.example.gleb.dentistcard.DatabaseRequest;
+import com.example.gleb.dentistcard.Pattern;
+import com.example.gleb.dentistcard.R;
+import com.example.gleb.insert.InsertCountry;
+import com.example.gleb.tables.Country;
+import com.mikepenz.materialdrawer.Drawer;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by gleb on 13.07.15.
+ */
+public class CountryFragment extends Fragment {
+    public static final String TAG = "TAG";
+    private DatabaseRequest request = new DatabaseRequest();
+    public CountryAdapter adapter;
+    public ImageButton imageButton;
+    public EditText oldCountryEditText;
+    public EditText newCountryEditText;
+    public String[] arrayCountry = null;
+    public int[] arrayIdCountry = null;
+
+    private List<Country> countries;
+    private RecyclerView rv;
+    public ImageButton addImageButton;
+    public ActionMode actionMode;
+    public ArrayList<Integer> positions;
+
+    private Drawer.Result drawerResult = null;
+
+    protected HttpClient client;
+    protected HttpPost post;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.countryactivity,container,false);
+
+        rv = (RecyclerView) v.findViewById(R.id.rv);
+        countries = new ArrayList<>();
+        addImageButton = (ImageButton) v.findViewById(R.id.addFloatingButton);
+        positions = new ArrayList<Integer>();
+
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        rv.setLayoutManager(llm);
+        rv.setHasFixedSize(true);
+
+        new Loader().execute();
+
+        addImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), InsertCountry.class);
+                startActivity(intent);
+            }
+        });
+
+        rv.addOnItemTouchListener(
+                new Pattern.RecyclerItemClickListener(getActivity(), new Pattern.RecyclerItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        boolean wrapInScrollView = true;
+                        MaterialDialog dialog = new MaterialDialog.Builder(getActivity())
+                                .title(R.string.UpdateCountry)
+                                .customView(R.layout.update_country, wrapInScrollView)
+                                .positiveText("Подтвердить")
+                                .negativeText("Отмена")
+                                .callback(new MaterialDialog.ButtonCallback() {
+                                    @Override
+                                    public void onNegative(MaterialDialog dialog) {
+                                        super.onNegative(dialog);
+                                    }
+
+                                    @Override
+                                    public void onPositive(MaterialDialog dialog) {
+                                        new Updater().execute();
+                                    }
+                                })
+                                .show();
+
+                        View v = dialog.getCustomView();
+
+                        oldCountryEditText = (EditText) v.findViewById(R.id.oldCountryEditText);
+                        oldCountryEditText.append(arrayCountry[position]);
+                        newCountryEditText = (EditText) v.findViewById(R.id.newCountryEditText);
+                    }
+
+                    @Override
+                    public void onItemLongPress(View childView, int position) {
+                        positions.add(position);
+                        actionMode = getActivity().startActionMode(callback);
+                    }
+                })
+        );
+
+        return v;
+    }
+
+    private ActionMode.Callback callback = new ActionMode.Callback() {
+
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            mode.getMenuInflater().inflate(R.menu.menu_header, menu);
+            return true;
+        }
+
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.menu_delete:
+                    for (int i = 0; i < positions.size(); i++){
+                        new Deleter(positions.get(i)).execute();
+
+                    }
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        public void onDestroyActionMode(ActionMode mode) {
+            Log.d(TAG, "destroy");
+            actionMode = null;
+        }
+
+    };
+
+    public class Loader extends AsyncTask<String, String, String[]> {
+
+        @Override
+        protected String[] doInBackground(String... params) {
+            //String with JSON
+            String jsonContent = request.makeRequest("http://dentists.16mb.com/SelectQuery/CountryScript.php");            //Fields of table CountriesActivity
+
+            Log.d(TAG, jsonContent);
+            try {
+                //create JSON array for parse it
+                JSONArray array = new JSONArray(jsonContent);
+                arrayCountry = new String[array.length()];
+                arrayIdCountry = new int[array.length()];
+
+                for (int i = 0; i < array.length(); i++){
+                    //parse of array
+                    JSONObject jObject = array.getJSONObject(i);
+                    int id = jObject.getInt("IdCountry");
+                    String country = jObject.getString("Country");
+                    arrayIdCountry[i] = id;
+                    arrayCountry[i] = country;
+
+                    countries.add(new Country(id, country));
+
+                    Log.d(TAG, arrayCountry[i]);
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return arrayCountry;
+        }
+
+        @Override
+        protected void onPostExecute(String[] value) {
+            adapter = new CountryAdapter(countries);
+            rv.setAdapter(adapter);
+
+        }
+    }
+
+    /**
+     * Update record from table CountriesActivity
+     */
+    public class Updater extends AsyncTask<String, String, String>{
+
+        @Override
+        protected String doInBackground(String... params) {
+            String oldCountry = oldCountryEditText.getText().toString();
+            String newCountry = newCountryEditText.getText().toString();
+            Log.d(TAG, oldCountry);
+
+            client = new DefaultHttpClient();
+            post = new HttpPost("http://dentists.16mb.com/UpdateScript/UpdateCountryScript.php");
+            HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000); // Timeout
+            HttpResponse response;
+            JSONObject json = new JSONObject();
+
+            try {
+                json.put("oldCountry", oldCountry);
+                json.put("newCountry", newCountry);
+                post.setHeader("json", json.toString());
+                StringEntity se = new StringEntity(json.toString());
+                se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+                post.setEntity(se);
+                response = client.execute(post);
+
+                if (response != null) {
+                    InputStream in = response.getEntity().getContent(); // Get the
+                    Log.i("Read from Server", in.toString());
+                }
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Toast.makeText(getActivity(), R.string.Send, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Delete record from table CountriesActivity
+     */
+    public class Deleter extends AsyncTask<String, String, String>{
+        private int positionCountry;
+
+        public Deleter(int positionCountry) {
+            this.positionCountry = positionCountry;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String country = arrayCountry[positionCountry];
+            Log.d(TAG, country);
+
+            client = new DefaultHttpClient();
+            post = new HttpPost("http://dentists.16mb.com/DeleteScript/DeleteCountryScript.php");
+            HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000); // Timeout
+            HttpResponse response;
+            JSONObject json = new JSONObject();
+
+            try {
+                json.put("countryPosition", country);
+                post.setHeader("json", json.toString());
+                StringEntity se = new StringEntity(json.toString());
+                se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+                post.setEntity(se);
+                response = client.execute(post);
+
+                if (response != null) {
+                    InputStream in = response.getEntity().getContent(); // Get the
+                    Log.i("Read from Server", in.toString());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Toast.makeText(getActivity(), R.string.Send, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
