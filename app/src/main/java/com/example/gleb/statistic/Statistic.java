5c278e707924f7d188287ea58d25ea66be5083f5
GlebKalinichenko <Glebjn@yandex.ua>
2015-08-06 10:01:54 +0300

    Activity for show numbers of worked particient for item doctor

--- app/src/main/java/com/example/gleb/statistic/Statistic.java
+++ app/src/main/java/com/example/gleb/statistic/Statistic.java
@@ -1,32 +1,46 @@
 package com.example.gleb.statistic;
 
+import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.ActionMode;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.gleb.connection.ConnectionRegistrator;
 import com.example.gleb.dentistcard.Login;
 import com.example.gleb.dentistcard.R;
+import com.example.gleb.fragments.SlidingTabLayout;
 import com.example.gleb.insert.InsertChange;
 import com.example.gleb.tables.Ticket;
+import com.mikepenz.iconics.typeface.FontAwesome;
 import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SectionDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.Badgeable;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -51,10 +65,10 @@ import java.util.Calendar;
 public class Statistic extends ActionBarActivity {
     public static final String TAG = "TAG";
     public static final String TICKET = "Ticket";
-    public TextView allTicketTextView;
-    public TextView fromDateTextView;
-    public TextView toDateTextView;
-    public TextView dateTextView;
+    public static final String ALLTICKET = "AllTicket";
+    public static final String FRESHTICKET = "FreshTicket";
+    public EditText fromDateEditText;
+    public EditText toDateEditText;
 
     protected HttpClient client;
     protected HttpPost post;
@@ -62,6 +76,8 @@ public class Statistic extends ActionBarActivity {
     public String fullName;
     public String workTicket;
     public String workTicketDate;
+    public int freshTicket;
+    public int allTicket;
 
     public Spinner dateSpinner;
     public String[] dates;
@@ -72,6 +88,12 @@ public class Statistic extends ActionBarActivity {
 
     public ImageButton addImageButton;
 
+    public Toolbar toolbar;
+    public ViewPager pager;
+    public SlidingTabLayout tabs;
+    public ActionMode actionMode;
+
+    public Drawer drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,12 +101,26 @@ public class Statistic extends ActionBarActivity {
         setContentView(R.layout.statistic);
 
         fullName = getIntent().getStringExtra(Statistic.TICKET);
+        freshTicket = getIntent().getIntExtra(Statistic.FRESHTICKET, 0);
+        allTicket = getIntent().getIntExtra(Statistic.ALLTICKET, 0);
         Log.d(TAG, "Statistic " + fullName);
 
-        allTicketTextView = (TextView) findViewById(R.id.allTicketTextView);
-        fromDateTextView = (TextView) findViewById(R.id.fromDateTextView);
-        toDateTextView = (TextView) findViewById(R.id.toDateTextView);
-        dateTextView = (TextView) findViewById(R.id.dateTextView);
+        new Statister().execute();
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(R.string.Changes);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Initialise Navigation Drawer
+        drawer = new Drawer();
+        drawer.withActivity(this);
+        drawer.withToolbar(toolbar);
+        drawer.withActionBarDrawerToggle(true);
+        drawer.withHeader(R.layout.drawer_header);
+
+        fromDateEditText = (EditText) findViewById(R.id.fromDateEditText);
+        toDateEditText = (EditText) findViewById(R.id.toDateEditText);
 
         addImageButton = (ImageButton) findViewById(R.id.addFloatingButton);
 
@@ -95,36 +131,19 @@ public class Statistic extends ActionBarActivity {
             }
         });
 
-        dates = new String[]{"from", "to"};
-//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, dates);
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        dateSpinner.setAdapter(adapter);
-//        Log.d(TAG, "Spinner " + dateSpinner.getPrompt());
-
-        spinner = (Button) findViewById(R.id.btnSpinnerPlanets);
-        spinner.setOnClickListener(new View.OnClickListener() {
+        fromDateEditText.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(Statistic.this, android.R.layout.simple_spinner_dropdown_item, dates);
-                new AlertDialog.Builder(Statistic.this).setTitle("the prompt").setAdapter(adapter, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        //((Button) findViewById(R.id.btnSpinnerPlanets)).setText(dates[which]);
-                        if (dates[which].equals("to")) {
-                            showDialog(1);
-                        }
-                        else{
-                            if (dates[which].equals("from")) {
-                                showDialog(0);
-                            }
-                        }
-                        dialog.dismiss();
-                    }
-                }).create().show();
+            public void onClick(View v) {
+                showDialog(0);
             }
         });
 
-        new Statister().execute();
+        toDateEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(1);
+            }
+        });
 
     }
 
@@ -147,7 +166,7 @@ public class Statistic extends ActionBarActivity {
         @Override
         public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
             fromDate = arg1 + "-" + arg2 + "-" + arg3;
-            fromDateTextView.setText(fromDate);
+            fromDateEditText.setText(fromDate);
             Log.d(TAG, "Date change from " + fromDate);
         }
     };
@@ -156,7 +175,7 @@ public class Statistic extends ActionBarActivity {
         @Override
         public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
             toDate = arg1 + "-" + arg2 + "-" + arg3;
-            toDateTextView.setText(toDate);
+            toDateEditText.setText(toDate);
             Log.d(TAG, "Date change to " + toDate);
 
         }
@@ -220,7 +239,65 @@ public class Statistic extends ActionBarActivity {
 
         @Override
         protected void onPostExecute(String[] value) {
-            allTicketTextView.setText(workTicket);
+            drawer.addDrawerItems(
+                    new PrimaryDrawerItem().withName(R.string.NewMessage).withIcon(FontAwesome.Icon.faw_home).withBadge("+" + freshTicket).withIdentifier(1),
+                    new PrimaryDrawerItem().withName(R.string.SendMessage).withIcon(FontAwesome.Icon.faw_gamepad).withBadge("+" + allTicket).withIdentifier(2),
+                    new PrimaryDrawerItem().withName(R.string.AllParticient).withIcon(FontAwesome.Icon.faw_eye).withBadge("+" + workTicket).withIdentifier(3),
+                    new SectionDrawerItem().withName(R.string.drawer_item_settings),
+                    new SecondaryDrawerItem().withName(R.string.drawer_item_help).withIcon(FontAwesome.Icon.faw_cog).withBadge("").withIdentifier(4),
+                    new SecondaryDrawerItem().withName(R.string.drawer_item_open_source).withIcon(FontAwesome.Icon.faw_question).setEnabled(false).withBadge("").withIdentifier(5),
+                    new DividerDrawerItem(),
+                    new SecondaryDrawerItem().withName(R.string.drawer_item_contact).withIcon(FontAwesome.Icon.faw_github).withBadge("12+").withIdentifier(6)
+            );
+
+            drawer.withOnDrawerListener(new Drawer.OnDrawerListener() {
+                @Override
+                public void onDrawerOpened(View drawerView) {
+                    // Скрываем клавиатуру при открытии Navigation Drawer
+                    InputMethodManager inputMethodManager = (InputMethodManager) Statistic.this.getSystemService(Activity.INPUT_METHOD_SERVICE);
+                    inputMethodManager.hideSoftInputFromWindow(Statistic.this.getCurrentFocus().getWindowToken(), 0);
+                }
+
+                @Override
+                public void onDrawerClosed(View drawerView) {
+                }
+            });
+
+            drawer.withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                @Override
+                // Обработка клика
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id, IDrawerItem drawerItem) {
+//                if (drawerItem instanceof Nameable) {
+//                    Toast.makeText(ParticientProfileActivity.this, ParticientProfileActivity.this.getString(((Nameable) drawerItem).getNameRes()), Toast.LENGTH_SHORT).show();
+//                }
+                    if (drawerItem instanceof Badgeable) {
+                        Badgeable badgeable = (Badgeable) drawerItem;
+                        int item = drawerItem.getIdentifier();
+
+                        switch (item) {
+                            case 1:
+                                break;
+                            case 2:
+//                            Intent intent = new Intent(Statistic.this, ConnectionRegistrator.class);
+//                            intent.putExtra(ConnectionRegistrator.EMAIL, emailParticient);
+//                            startActivity(intent);
+                                break;
+                        }
+                    }
+                }
+            });
+
+            drawer.withOnDrawerItemLongClickListener(new Drawer.OnDrawerItemLongClickListener() {
+                @Override
+                // Обработка длинного клика, например, только для SecondaryDrawerItem
+                public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id, IDrawerItem drawerItem) {
+                    if (drawerItem instanceof SecondaryDrawerItem) {
+                        Toast.makeText(Statistic.this, Statistic.this.getString(((SecondaryDrawerItem) drawerItem).getNameRes()), Toast.LENGTH_SHORT).show();
+                    }
+                    return false;
+                }
+            });
+            drawer.build();
         }
     }
 
@@ -284,9 +361,8 @@ public class Statistic extends ActionBarActivity {
 
         @Override
         protected void onPostExecute(String[] value) {
-            dateTextView.setText(workTicketDate);
+            Toast.makeText(getBaseContext(), "Количество пациентов: " + workTicketDate + " за период с " + fromDate + " по " + toDate, Toast.LENGTH_LONG).show();
         }
     }
-
 
 }
