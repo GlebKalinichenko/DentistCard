ddd546183a06d19f928b4cb3a17f264ce634bb28
GlebKalinichenko <Glebjn@yandex.ua>
2015-08-06 10:05:48 +0300

    Adapter for make toggle of between different fragments in doctor profile

--- app/src/main/java/com/example/gleb/viewpagers/DoctorViewPagerAdapter.java
+++ app/src/main/java/com/example/gleb/viewpagers/DoctorViewPagerAdapter.java
@@ -5,12 +5,10 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.util.Log;
 
-import com.example.gleb.fragments.DoctorFragment;
-import com.example.gleb.fragments.ParticientDoctorFragment;
-import com.example.gleb.fragments.ParticientFragment;
-import com.example.gleb.fragments.RecomendationDoctorFragment;
-import com.example.gleb.fragments.RecomendationFragment;
-import com.example.gleb.fragments.TicketDoctorFragment;
+import com.example.gleb.fragments.DoctorFragmentDoctorProfile;
+import com.example.gleb.fragments.ParticientDoctorProfile;
+import com.example.gleb.fragments.RecomendationFragmentDoctorProfile;
+import com.example.gleb.fragments.TicketDoctorProfile;
 
 /**
  * Created by gleb on 14.07.15.
@@ -20,14 +18,18 @@ public class DoctorViewPagerAdapter extends FragmentStatePagerAdapter {
     private CharSequence Titles[]; // This will Store the Titles of the Tabs which are Going to be passed when AdminViewPagerAdapter is created
     private int NumbOfTabs; // Store the number of tabs, this will also be passed when the AdminViewPagerAdapter is created
     private String fullName;
+    public int freshTicket;
+    public int allTicket;
 
     // Build a Constructor and assign the passed Values to appropriate values in the class
-    public DoctorViewPagerAdapter(FragmentManager fm, CharSequence mTitles[], int mNumbOfTabsumb, String fullName) {
+    public DoctorViewPagerAdapter(FragmentManager fm, CharSequence mTitles[], int mNumbOfTabsumb, String fullName, int freshTicket, int allTicket) {
         super(fm);
 
         this.Titles = mTitles;
         this.NumbOfTabs = mNumbOfTabsumb;
         this.fullName = fullName;
+        this.freshTicket = freshTicket;
+        this.allTicket = allTicket;
         Log.d(TAG, "DoctorViewPagerAdapter " + fullName);
 
     }
@@ -35,26 +37,26 @@ public class DoctorViewPagerAdapter extends FragmentStatePagerAdapter {
     //This method return the fragment for the every position in the View Pager
     @Override
     public Fragment getItem(int position) {
-
+        String profile = "doctor";
 
         if (position == 0) // if the position is 0 we are returning the First tab
         {
-            DoctorFragment tab1 = new DoctorFragment();
+            DoctorFragmentDoctorProfile tab1 = new DoctorFragmentDoctorProfile(fullName, freshTicket, allTicket, profile);
             return tab1;
         } else             // As we are having 2 tabs if the position is now 0 it must be 1 so we are returning second tab
         {
             if (position == 1) // if the position is 0 we are returning the First tab
             {
-                ParticientDoctorFragment tab2 = new ParticientDoctorFragment(fullName);
+                ParticientDoctorProfile tab2 = new ParticientDoctorProfile(fullName, freshTicket, allTicket, profile);
                 return tab2;
             } else {
                 if (position == 2) // if the position is 0 we are returning the First tab
                 {
-                    TicketDoctorFragment tab3 = new TicketDoctorFragment(fullName);
+                    TicketDoctorProfile tab3 = new TicketDoctorProfile(fullName, freshTicket, allTicket, profile);
                     return tab3;
                 }
             }
-            RecomendationDoctorFragment tab4 = new RecomendationDoctorFragment(fullName);
+            RecomendationFragmentDoctorProfile tab4 = new RecomendationFragmentDoctorProfile(fullName, freshTicket, allTicket, profile);
             return tab4;
         }
     }
